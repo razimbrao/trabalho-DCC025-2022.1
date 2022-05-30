6484cbd42624469b747b2bc62e283d3d645c74dd
AngryBeanTrevota <58980330+AngryBeanTrevota@users.noreply.github.com>
2022-05-30 11:02:14 -0300

    Método que simula a partida entre bots concluido

--- jokenpo/src/main/java/com/trabalho/Partida.java
+++ jokenpo/src/main/java/com/trabalho/Partida.java
@@ -19,31 +19,108 @@ public class Partida {
         // Cria loop do melhor de três
         
         int i = 0;
-        while(i < 3 || (pontuacaoJ1 != 0 && pontuacaoJ2 !=0))
+        while(i < 3 || (pontuacaoJ1 == 0 && pontuacaoJ2 ==0))
         {
             String[] resultados = {"pedra", "papel", "tesoura", "lagarto", "spock"};
             
-            // Seleciona o valor de cada um aleatoriamente 
+            int jogadaJ1 = (int)((0 + (5 - 0)) * Math.random());
+            int jogadaJ2 = (int)((0 + (5 - 0)) * Math.random());
+            //String[] jogadas = {jogadaJ1, jogadaJ2};
             
-            String jogadaJ1 = "tesoura"; //resultados[(int)((0 + (5 - 0)) * Math.random())];
-            String jogadaJ2 = "papel"; //resultados[(int)((0 + (5 - 0)) * Math.random())];
-            String[] jogadas = {jogadaJ1, jogadaJ2};
+            System.out.println(j1.getNome() + ": " + resultados[jogadaJ1] + " // " + j2.getNome() + ": " + resultados[jogadaJ2]);
             
-            System.out.println("j1: " + jogadaJ1 + " j2: " + jogadaJ2);
+            // switch case com todas as possiblidades
             
-            // dicionário com os resultados possíveis
-            
-            //HashMap<String[], Integer[]> relacoesEntreJogadas = new HashMap<>();
-            //relacoesEntreJogadas.put({"tesoura", "papel"}, {1, 1});
-                
-            // atualiza loop
+            switch((jogadaJ1*10) + jogadaJ2)
+            {
+                case 21:
+                    System.out.println("Tesoura corta papel, jogador " + j1.getNome() + " ganha com tesoura");
+                    pontuacaoJ1++;
+                    break;
+                case 12:
+                    System.out.println("Tesoura corta papel, jogador " + j2.getNome() + " ganha com tesoura");
+                    pontuacaoJ2++;
+                    break;
+                case 10:
+                    System.out.println("Papel cobre pedra, jogador " + j1.getNome() + " ganha com papel");
+                    pontuacaoJ1++;
+                    break;
+                case 1:
+                    System.out.println("Papel cobre pedra, jogador " + j2.getNome() + " ganha com papel");
+                    pontuacaoJ2++;
+                    break;                    
+                case 3:
+                    System.out.println("Pedra esmaga lagarto, jogador " + j1.getNome() + " ganha com pedra");
+                    pontuacaoJ1++;
+                    break;
+                case 30:
+                    System.out.println("Pedra esmaga lagarto, jogador " + j2.getNome() + " ganha com pedra");
+                    pontuacaoJ2++;
+                    break; 
+                case 34:
+                    System.out.println("Lagarto evenena Spock, jogador " + j1.getNome() + " ganha com lagarto");
+                    pontuacaoJ1++;
+                    break;
+                case 43:
+                    System.out.println("Lagarto evenena Spock, jogador " + j2.getNome() + " ganha com lagarto");
+                    pontuacaoJ2++;
+                    break;        
+                case 42:
+                    System.out.println("Spock amassa tesoura, jogador " + j1.getNome() + " ganha com Spock");
+                    pontuacaoJ1++;
+                    break;
+                case 24:
+                    System.out.println("Spock amassa tesoura, jogador " + j2.getNome() + " ganha com Spock");
+                    pontuacaoJ2++;
+                    break; 
+                case 23:
+                    System.out.println("Tesoura decapita lagarto, jogador " + j1.getNome() + " ganha com tesoura");
+                    pontuacaoJ1++;
+                    break;
+                case 32:
+                    System.out.println("Tesoura decapita lagarto, jogador " + j2.getNome() + " ganha com tesoura");
+                    pontuacaoJ2++;
+                    break;     
+                case 31:
+                    System.out.println("Lagarto come papel, jogador " + j1.getNome() + " ganha com lagarto");
+                    pontuacaoJ1++;
+                    break;
+                case 13:
+                    System.out.println("Lagarto come papel, jogador " + j2.getNome() + " ganha com lagarto");
+                    pontuacaoJ2++;
+                    break;   
+                case 14:
+                    System.out.println("Papel desmente Spock, jogador " + j1.getNome() + " ganha com papel");
+                    pontuacaoJ1++;
+                    break;
+                case 41:
+                    System.out.println("Papel desmente Spock, jogador " + j2.getNome() + " ganha com papel");
+                    pontuacaoJ2++;
+                    break;    
+                case 40:
+                    System.out.println("Spock pulveriza pedra, jogador " + j1.getNome() + " ganha com Spock");
+                    pontuacaoJ1++;
+                    break;
+                case 4:
+                    System.out.println("Spock pulveriza pedra, jogador " + j2.getNome() + " ganha com Spock");
+                    pontuacaoJ2++;
+                    break; 
+                case 2:
+                    System.out.println("Pedra amassa tesoura, jogador " + j1.getNome() + " ganha com pedra");
+                    pontuacaoJ1++;
+                    break;
+                case 20:
+                    System.out.println("Pedra amassa tesoura, jogador " + j2.getNome() + " ganha com pedra");
+                    pontuacaoJ2++;
+                    break;                     
+                default:
+                    System.out.println("Erro");
+                    System.out.println(pontuacaoJ1 + " " + pontuacaoJ2);
+            }
                 
             i++;
         }
-        
-        // Imprime e retorna o jogador 
-        
-        
+          
         if(pontuacaoJ1 > pontuacaoJ2)
         {
             vencedor = j1;
@@ -53,8 +130,10 @@ public class Partida {
             vencedor = j2;
         }
         
-        System.out.println("O vencedor eh " + j1.getNome());
-        return j1;
+        System.out.println("O vencedor eh " + vencedor.getNome());
+        imprimePlacar();
+        
+        return vencedor;
     }
 
     public void imprimePlacar(){
