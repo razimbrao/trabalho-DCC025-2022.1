9f7adf4970f98673060230d20268e12068a221e3
JoViFernandes <joaovitor.fernandes@estudante.ufjf.br>
2022-08-01 15:05:41 -0300

    adicionando list de vitorias

--- jokenpo/src/main/java/com/trabalho/Campeonato.java
+++ jokenpo/src/main/java/com/trabalho/Campeonato.java
@@ -41,7 +41,9 @@ public class Campeonato {
     private List<Jogador> listaJogadoresAux = new ArrayList<>();
     private List<Partida> asaEsquerda = new ArrayList<>();
     private List<Partida> asaDireita = new ArrayList<>();
-
+    
+    private List<Jogador> listaVitorias = new ArrayList<>();
+    
     public Campeonato(int n) { // informa qual o tamanho do campeonato
         this.tamanho = n;
         String[] opcoesJogo = { "Simular", "Jogar" };
